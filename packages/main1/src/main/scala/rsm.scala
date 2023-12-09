4fad096b4f255ab865b1b3ad3d58f81db1f7bd01
me <meh@meh>
2023-12-09 23:29:05 +0700

    apply these changes

--- packages/main1/src/main/scala/rsm.scala
+++ packages/main1/src/main/scala/rsm.scala
@@ -7,13 +7,67 @@
 
 
 
+import lscalg.cli.repl.UReadlineConsole
+
+
 
 
 
 @main
 def runSMain() : Unit
-= {}
+= {
+  ;
+}
 
+@main
+def runRlEcho() : Unit
+= {
+  import lscalg.cli.repl.fvmPosixReplConsoleImplicits.{given UReadlineConsole._Any }
+
+  for (_ <- Range(0, 4) )
+  do {
+    val txt = {
+      ;
+      summon[UReadlineConsole._Any ]
+      .promptAndReadLine(">")
+    }
+    println(s"[out] $txt ")
+  }
+}
+
+@main
+def runRlLs() : Unit
+= {
+  ;
+
+  import language.unsafeNulls
+
+  import lscalg.cli.repl.UReadLineList
+
+  val sRl
+  = {
+    ;
+
+    import language.unsafeNulls
+
+    new java.io.BufferedReader(new java.io.InputStreamReader(System.in ) ) {
+      ;
+      override
+      def readLine()
+      : String | Null
+      = {
+        System.out.print("> ")
+        super.readLine()
+      }
+    }
+  }
+
+  for ((lC) <- UReadLineList.apply(sRl ).take(5 ) )
+  do {
+    ;
+    println(s"[out] $lC ")
+  }
+}
 
 
 
