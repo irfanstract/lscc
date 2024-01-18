805dddcee77eaa0212143e2e2a95c955cbc8f5eb
me <meh@meh>
2024-01-18 16:28:36 +0700

    apply these changes

--- packages/kscLang_grammar/src/main/scala/lscc/spclParsedConstructs1/ForValDecl.scala
+++ packages/kscLang_grammar/src/main/scala/lscc/spclParsedConstructs1/ForValDecl.scala
@@ -19,135 +19,10 @@ package lscc.spclParsedConstructs1
 
 ;
 
-object ForPrefixedExpr {
-  ;
-
-  def apply
-    //
-    (using givenFispoSupp : lscalg.bnfParsing.spclCommonLookaheadCaps1.GivenFispoSupp._Any )
-    (using apMode : SpclApm.withPtrTRange[givenFispoSupp.T, givenFispoSupp.T ] )
-    (using kwIngCtx : lscc.spclParsedConstructs1.KeywordingCtx.WithGivenFispoSupp[givenFispoSupp.type ] )
-    (using lscalg.bnfParsing.spclCommonLookaheadCaps.ForImmediatePatterOccurence._AnyForReceiverAndSpecAndReturnBaseType[givenFispoSupp.T, util.matching.Regex, givenFispoSupp.SpclAfterDigestTupleOption._Any ] )
-    ()
-  = {
-    (
-      ForPrefixedScrutTermLvlExpr()
-    )
-  }
-
-  ;
-}
-
 
 
 
 ;
-
-object ForPrefixedScrutTermLvlExpr
-{
-  ;
-
-  // val kwIngCtx
-  // = ForTermLevelUnprefixedKeyword()
-
-  ;
-
-  /**
-   * `val example1 @ (expr)`, `val exampleSome : T`
-   * 
-   */
-  def apply
-    //
-    (using givenFispoSupp : lscalg.bnfParsing.spclCommonLookaheadCaps1.GivenFispoSupp._Any )
-    (using apMode : SpclApm.withPtrTRange[givenFispoSupp.T, givenFispoSupp.T ] )
-    (using kwIngCtx : lscc.spclParsedConstructs1.KeywordingCtx.WithGivenFispoSupp[givenFispoSupp.type ] )
-    (using lscalg.bnfParsing.spclCommonLookaheadCaps.ForImmediatePatterOccurence._AnyForReceiverAndSpecAndReturnBaseType[givenFispoSupp.T, util.matching.Regex, givenFispoSupp.SpclAfterDigestTupleOption._Any ] )
-    ()
-  // : lscalg.digestivity.ParseFunction.ForReceiverAndRValue[givenFispoSupp.T , FvdParsed._Any ]
-  = {
-    ;
-
-    import givenFispoSupp.T as PAny
-    import lscalg.parsing.Subject.returnedMainValueMapOpExtras.returnedMainValueMapOp1
-    import lscalg.parsing.Subject.returnedMainValueMapOpExtras.returnedMainValueWithFinalPosMapOps1
-
-    ({
-      ;
-
-
-      ({
-        ;
-
-        ({
-          ;
-
-          import fwscImplicits.prsWhitespaceableHeadTailConcatOp
-
-          (
-            ForPrefixingScrutTermLvlExpr()
-
-            +%:
-
-            ForScrutSyntactic()
-
-            +%:
-
-            lscalg.parsing.ParseFunction.emptyTupleValuedInstance[PAny]
-          )
-          .map({ case (bindingExpr, s) => bindingExpr } )
-        })
-      })
-    })
-  }
-
-  ;
-}
-
-object ForPrefixingScrutTermLvlExpr {
-  ;
-
-  def apply
-    //
-    (using givenFispoSupp : lscalg.bnfParsing.spclCommonLookaheadCaps1.GivenFispoSupp._Any )
-    (using apMode : SpclApm.withPtrTRange[givenFispoSupp.T, givenFispoSupp.T ] )
-    (using kwIngCtx : lscc.spclParsedConstructs1.KeywordingCtx.WithGivenFispoSupp[givenFispoSupp.type ] )
-    (using lscalg.bnfParsing.spclCommonLookaheadCaps.ForImmediatePatterOccurence._AnyForReceiverAndSpecAndReturnBaseType[givenFispoSupp.T, util.matching.Regex, givenFispoSupp.SpclAfterDigestTupleOption._Any ] )
-    ()
-  = {
-    ;
-
-    import givenFispoSupp.T as PAny
-    import lscalg.parsing.Subject.returnedMainValueMapOpExtras.returnedMainValueMapOp1
-    import lscalg.parsing.Subject.returnedMainValueMapOpExtras.returnedMainValueWithFinalPosMapOps1
-    
-    ;
-
-    (
-      ForValDefOnly()
-      .map(e => {
-        e.map(PrefixScrutLhsExpr.ForValDef.apply _ )
-      } )
-
-      orElse
-
-      ForUnderscoreWildcardDefOnly()
-      .map(e => {
-        e.map(_ => PrefixScrutLhsExpr.ForIgnorableWildcard() )
-      } )
-    )
-  }
-
-  ;
-}
-
-enum PrefixScrutLhsExpr {
-  ;
-
-  case ForIgnorableWildcard()
-
-  case ForValDef(value: FvdParsed._Any )
-
-}
 
 
 
@@ -172,16 +47,28 @@ object ForUnderscoreWildcardDefOnly {
     (using kwIngCtx : lscc.spclParsedConstructs1.KeywordingCtx.WithGivenFispoSupp[givenFispoSupp.type ] )
     (using lscalg.bnfParsing.spclCommonLookaheadCaps.ForImmediatePatterOccurence._AnyForReceiverAndSpecAndReturnBaseType[givenFispoSupp.T, util.matching.Regex, givenFispoSupp.SpclAfterDigestTupleOption._Any ] )
     ()
+  : lscalg.digestivity.ParseFunction.ForReceiverAndRValue[givenFispoSupp.T , apMode.AfterSi[UnderscoreWildcardPatternImpl._Any ] ]
   = (
     ForUnderscoreWildcardPattern.apply()
     .mapWithFinalPtrPosVl((vl, pos) => {
-      vl
+      UnderscoreWildcardPatternImpl.forExactSrcLevelToken(vl )
       .withSrcInfo(srcPosInfo = pos )
     } )
+    .nn
   )
 
   ;
 }
+
+object UnderscoreWildcardPatternImpl {
+  ;
+  type _Any
+  = UnderscoreWildcardPatternImpl[?]
+  def forExactSrcLevelToken(value: String)
+  : _Any
+  = new UnderscoreWildcardPatternImpl()
+}
+case class UnderscoreWildcardPatternImpl[T] private [UnderscoreWildcardPatternImpl] ()
 
 
 
@@ -239,7 +126,7 @@ object ForValDefOnly
 
             +%:
 
-            // ForScrutSyntactic()
+            // ForScrutOperatorAndRhs()
 
             // +%:
               
